e1eb9ddbcedb44cd9ef4487617a671e9e6158758
Rak65 <rakeshp6505@gmail.com>
2023-11-16 13:30:57 +0530

    [Added] : Add multiple person to address book.

--- addressbook/AddressBook.java
+++ addressbook/AddressBook.java
@@ -1,8 +1,35 @@
 package com.addressbook;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
     public static void main(String[] args) {
-        System.out.println("Give the information for First Contact");
-        Contact contact =new Contact();
-        contact.Add_Contact();
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Contact> list=new ArrayList<>();
+        int choice;
+        boolean isValid = true;
+        while (isValid){
+            Contact contact =new Contact();
+            System.out.println("Choose your option : ");
+            System.out.println("1. Add Contact");
+            System.out.println("2. Display all contact");
+            System.out.println("3. Exit");
+            choice = scanner.nextInt();
+            switch (choice){
+                case 1 :
+                    contact.Add_Contact(list);
+                    break;
+                case 2 :
+                    contact.DisplayContacts(list);
+                    break;
+                case 3 :
+                    isValid = false;
+                    break;
+                default:
+                    System.out.println("Enter valid option.");
+                    break;
+            }
+        }
     }
 }

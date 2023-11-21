bba04854842ce9675d622b89988ffe8dc4880bfe
Rak65 <rakeshp6505@gmail.com>
2023-11-22 00:47:12 +0530

    [Added] :- Refactor to add multiple address book to the system. Each Address book has a unique name.

--- addressbook/AddressBook.java
+++ addressbook/AddressBook.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Contact> list=new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+    ArrayList<Contact> list=new ArrayList<>();
+    public void operate() {
         int choice;
         boolean isValid = true;
-        while (isValid){
-            Contact contact =new Contact();
+        while (isValid) {
+            Contact contact = new Contact();
             System.out.println("Choose your option : ");
             System.out.println("1. Add Contact");
             System.out.println("2. Display all contact");
@@ -18,20 +18,20 @@ public class AddressBook {
             System.out.println("4. Delete contact.");
             System.out.println("5. Exit");
             choice = scanner.nextInt();
-            switch (choice){
-                case 1 :
+            switch (choice) {
+                case 1:
                     contact.addContact(list);
                     break;
-                case 2 :
+                case 2:
                     contact.displayContacts(list);
                     break;
-                case 3 :
+                case 3:
                     System.out.println("Search contact which you want to edit :");
                     System.out.println("Enter firstname : ");
                     String firstName = scanner.next();
                     System.out.println("Enter lastname : ");
                     String lastNAme = scanner.next();
-                    contact.editContacts(list,firstName,lastNAme);
+                    contact.editContacts(list, firstName, lastNAme);
                     break;
                 case 4:
                     System.out.println("Enter firstname to delete: ");
@@ -40,7 +40,7 @@ public class AddressBook {
                     String deleteLastName = scanner.next();
                     contact.deleteContact(list, deleteFirstName, deleteLastName);
                     break;
-                case 5 :
+                case 5:
                     isValid = false;
                     break;
                 default:
